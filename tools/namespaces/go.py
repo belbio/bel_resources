684388286b4aaf2ced19dd892733aee833f6bfe5
William Hayes <william.s.hayes@gmail.com>
2018-08-15 10:10:41 -0400

    re-added complex type to GO

--- tools/namespaces/go.py
+++ tools/namespaces/go.py
@@ -78,87 +78,6 @@ def update_data_files() -> bool:
     return changed_flag
 
 
-def add_complex_entity_type(start_id: str, ontology: Mapping[str, Any]) -> None:
-    """Add complex entity type to children of GO:0032991 -- macromolecular complex
-
-    Args:
-        start_id (int): parent id of GO terms of complexes
-
-    Returns:
-        None
-    """
-
-    ontology[start_id].other['entity_type'] = 'Complex'
-
-    for c in ontology[start_id].children:
-        add_complex_entity_type(c.id, ontology)
-
-
-def old_process_obo(force: bool = False):
-
-    # Terminology JSONL output filename
-    data_fp = config["bel_resources"]["file_locations"]["data"]
-    terms_fp = f'{data_fp}/namespaces/{namespace_key}.jsonl.gz'
-
-    # Don't rebuild file if it's newer than downloaded source file
-    if not force:
-        if utils.file_newer(terms_fp, local_data_fp):
-            log.info('Will not rebuild data file as it is newer than downloaded source file')
-            return False
-
-    with gzip.open(local_data_fp, 'r') as fi, gzip.open(terms_fp, 'wt') as fo:
-
-        # Header JSONL record for terminology
-        metadata = get_metadata()
-        fo.write("{}\n".format(json.dumps({'metadata': metadata})))
-
-        ont = pronto.Ontology(fi)
-        add_complex_entity_type('GO:0032991', ont)
-        unique_names = {}
-
-        for ont_term in ont:
-            if 'is_obsolete' in ont_term.other:
-                continue
-
-            src_id = ont_term.id.replace('GO:', '')
-            term = {
-                'namespace': ns_prefix,
-                'namespace_value': ont_term.name,
-                'src_id': src_id,
-                'id': utils.get_prefixed_id(ns_prefix, ont_term.name),
-                'alt_ids': [utils.get_prefixed_id(ns_prefix, src_id)],
-                'label': ont_term.name,
-                'name': ont_term.name,
-                'description': ont_term.desc,
-                'synonyms': [],
-                'children': [],
-                'entity_types': [],
-            }
-            if ont_term.name not in unique_names:
-                unique_names[ont_term.name] = 1
-            else:
-                log.error(f'Duplicate name in GO: {ont_term.name}')
-
-            subtree = ont_term.other['namespace']
-            if 'entity_type' in ont_term.other:
-                term['entity_types'].append(ont_term.other['entity_type'])
-            elif 'biological_process' in subtree:
-                term['entity_types'].append('BiologicalProcess')
-            elif 'cellular_component' in subtree:
-                term['entity_types'].append('Location')
-            elif 'molecular_function' in subtree:
-                term['entity_types'].append('Activity')
-
-            for syn in ont_term.synonyms:
-                term['synonyms'].append(syn.desc)
-
-            for c in ont_term.children:
-                term['children'].append(c.id.replace('GO:', ''))
-
-            # Add term to JSONL
-            fo.write("{}\n".format(json.dumps({'term': term})))
-
-
 def process_obo(force: bool = False):
 
     # Terminology JSONL output filename
@@ -263,14 +182,15 @@ def process_obo(force: bool = False):
                         term['entity_types'].append('Activity')
 
                 elif key == 'alt_id':
-                    val = val.replace('DOID', 'DO').strip()
                     term['alt_ids'].append(val)
 
                 elif key == 'is_a':
-                    matches = re.match('DOID:(\d+)\s', val)
+                    matches = re.match('(GO:\d+)\s', val)
                     if matches:
                         parent_id = matches.group(1)
-                        term['parents'].append(f'DO:{parent_id}')
+                        term['parents'].append(f'{parent_id}')
+                        if parent_id == 'GO:0032991':
+                            term['entity_types'] = ['Complex']
 
 
 def main():
