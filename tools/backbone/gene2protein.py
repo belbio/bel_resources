84b61bd0e9d717207c1fff9c44a33b6223619033
William Hayes <whayes@biodati.com>
2018-10-16 12:24:29 +0000

    added published to backbones

--- tools/backbone/gene2protein.py
+++ tools/backbone/gene2protein.py
@@ -77,7 +77,7 @@ def process_backbone():
                 'citation': {'uri': f'{src_url}{src_id}'},
                 'assertions': assertions,
                 'annotations': [{'type': 'Species', 'id': species, 'label': species_label}],
-                'metadata': {},
+                'metadata': {"gd:published": True},
             }
 
             if species in hmrz_species:
